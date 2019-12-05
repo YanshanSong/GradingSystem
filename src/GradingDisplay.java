75092be9e94bdc7250ffbf0642403a5859ef36cc
宋彦杉 <songyanshan.2008@163.com>
2019-12-04 21:57:29 -0500

    123

--- src/GradingDisplay.java
+++ src/GradingDisplay.java
@@ -1,103 +1,31 @@
 import java.awt.*;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-
 import javax.swing.*;
 
-public class GradingDisplay extends JFrame {
-    private int width;
-    private int height;
-    private JPanel rootPanel;
+public class GradingDisplay extends JFrame{
+    private JPanel jPanelTopLeft, jPanelTopRight, jPanelBottomLeft, jPanelBottomRight;
 
-    public static FocusListener focusListenerText = new FocusListener() {
-        @Override
-        public void focusGained(FocusEvent focusEvent) {
-            JTextField jTextField = (JTextField) focusEvent.getSource();
-            if(jTextField.getText().equals(jTextField.getName())) jTextField.setText("");
-        }
 
-        @Override
-        public void focusLost(FocusEvent focusEvent) {
-            JTextField jTextField = (JTextField) focusEvent.getSource();
-            if(jTextField.getText().length() == 0){
-                jTextField.setText(jTextField.getName());
-            }
-        }
-    };
-
-    public static FocusListener focusListenerPassword = new FocusListener() {
-        @Override
-        public void focusGained(FocusEvent focusEvent) {
-            JPasswordField jPasswordField = (JPasswordField) focusEvent.getSource();
-            String pass = String.valueOf(jPasswordField.getPassword());
-            if(pass.equals(jPasswordField.getName())){
-                jPasswordField.setEchoChar('*');
-                jPasswordField.setText("");
-            }
-        }
-
-        @Override
-        public void focusLost(FocusEvent focusEvent) {
-            JPasswordField jPasswordField = (JPasswordField) focusEvent.getSource();
-            String pass = String.valueOf(jPasswordField.getPassword());
-            if(pass.length() == 0){
-                jPasswordField.setEchoChar((char)0);
-                jPasswordField.setText(jPasswordField.getName());
-            }
-        }
-    };
-
-    public void changeDisplay(JPanel jPanel){
-        this.rootPanel.removeAll();
-        this.rootPanel.add(jPanel, BorderLayout.CENTER);
-        this.rootPanel.revalidate();
-        this.rootPanel.repaint();
-    }
-
-    public void welcome(){
-        WelcomeDisplay welcomeDisplay = new WelcomeDisplay();
-        FormDisplay formDisplay = new FormDisplay(welcomeDisplay.getComponents());
-        this.changeDisplay(formDisplay);
-    }
-
-    public void user(User user){
-        UserDisplay userDisplay = new UserDisplay(user);
-        TabbedDisplay tabbedDisplay = new TabbedDisplay(userDisplay.getComponents(), userDisplay.getStrings(), userDisplay.getTop());
-        this.changeDisplay(tabbedDisplay);
-    }
-
-    public void login(){
-        LoginDisplay loginDisplay = new LoginDisplay();
-        FormDisplay formDisplay = new FormDisplay(loginDisplay.getComponents());
-        this.changeDisplay(formDisplay);
-    }
-
-    public void template(Template template, int index){
-        TemplateDisplay templateDisplay = new TemplateDisplay(template, index);
-        TwoTableView twoTableView = new TwoTableView(templateDisplay.getLeft(), templateDisplay.getRight(), "Template", templateDisplay.getOptions());
-        this.changeDisplay(twoTableView);
-    }
-
-    public void course(Course course){
-        CourseDisplay courseDisplay = new CourseDisplay(course);
-        TabbedDisplay tabbedDisplay = new TabbedDisplay(courseDisplay.getComponents(), courseDisplay.getStrings(), courseDisplay.getTop());
-        this.changeDisplay(tabbedDisplay);
-    }
     /**
      * Create the frame.
      */
-    public GradingDisplay(GradingSystemBackend gradingSystemBackend, int width, int height) throws Exception {
-        this.width = width;
-        this.height = height;
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle(gradingSystemBackend.getName());
-        this.setVisible(true);
-        this.setSize(width, height);
-        this.rootPanel = new JPanel();
-        this.rootPanel.setSize(width, height);
-        this.rootPanel.setLayout(new BorderLayout());
-        this.add(rootPanel);
+    public GradingDisplay(int width, int height) {
+        setSize(width, height);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
 
-        this.welcome();
+        jPanelTopLeft = new JPanel();
+		jPanelTopLeft.setLayout(new BorderLayout());
+		ScaleIcon scaleIcon1 = new ScaleIcon(new ImageIcon("img/BU-logo.png"));
+		JLabel jLabel1 = new JLabel(scaleIcon1);
+		jPanelTopLeft.add(jLabel1, BorderLayout.CENTER);
+
+		jPanelTopRight = new JPanel();
+		jPanelTopRight.setLayout(new BorderLayout());
+		ScaleIcon scaleIcon2 = new ScaleIcon(new ImageIcon("img/Charles-River.jpg"));
+		JLabel jLabel2 = new JLabel(scaleIcon2);
+		jPanelTopRight.add(jLabel2, BorderLayout.CENTER);
+
+		JSplitPane jSplitPanelTop = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, jPanelTopLeft, jPanelTopRight);
+		jSplitPanelTop.setEnabled(false);
     }
 }
---
+++ src_old/Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class Main {
+    /**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) throws Exception {
+		GradingSystem.start();
+	}
+}
