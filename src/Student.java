6528cffdabd54155c63f0e931ccc37a19d9ddc3b
yangchenhongyi <yangchenhongyi@gmail.com>
2019-12-05 15:02:09 -0500

    add importing student from .txt files

--- src/Student.java
+++ src/Student.java
@@ -6,4 +6,10 @@ public class Student {
         this.name = name;
         this.id = id;
     }
+
+    Student(String stu_str){
+        String[] attrs = stu_str.split(" ", 3);
+        this.name = attrs[0];
+        this.id = attrs[1];
+    }
 }
---
+++ src/StudentList.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.io.*;
+
+
+public class StudentList {
+    private ArrayList<Student> students;
+
+    StudentList(String filename) throws IOException {
+        this.students = new ArrayList<Student>();
+        File file = new File(filename);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            Student new_student = new Student(line);
+            this.students.add(new_student);
+        }
+    }
+}
+
+
+
